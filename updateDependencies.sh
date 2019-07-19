cef117e103e609b384516f72c86f4344055ce1ca
Fabio Montezuma <fabio.montezuma@soaexpert.com.br>
2019-07-19 14:38:26 -0300

    Change updateDependencies process

--- updateDependencies.sh
+++ updateDependencies.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
-for dir in helm-chart-sources/*; do (helm package -u "$dir"); done
+echo "Creating new packages"
+mkdir newpackages && cd newpackages
+for dir in ../helm-chart-sources/*; do (helm package -u "$dir"); done
+echo ""
+echo "Moving new packages to root"
+mv -vn * ../
+cd .. && rm -rf newpackages
+echo ""
+echo "Indexing packages"
 helm repo index --url https://fmontezuma.github.io/helm-chart/ .

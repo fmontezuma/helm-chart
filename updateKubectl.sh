3d86d84247b93bf921df6c35fcc35bfc68232d58
Fabio Montezuma <fabio.montezuma@soaexpert.com.br>
2019-08-14 13:24:08 -0300

    Update Istio setup

--- updateKubectl.sh
+++ updateKubectl.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 kubectl apply -f ./k8s-common/charts/istio/templates/01-istio-system-ns.yaml
 kubectl apply -f ./k8s-common/charts/istio/templates/02-istio-init.yml
-kubectl apply -f ./k8s-common/charts/istio/templates/03-istio.yml
+kubectl apply -f ./k8s-common/charts/istio/templates/03-istio-cni.yml
+kubectl apply -f ./k8s-common/charts/istio/templates/04-istio.yml
 kubectl apply -f . -R
